2c5bb5ca4207879aa64a3eabbdd92531a50492a0
akshayp-mangospring <akshayp@mangospring.com>
2022-06-03 12:22:15 +0530

    Add akshay folder for test cases

--- cypress/e2e/akshay/login.cy.js
+++ cypress/e2e/akshay/login.cy.js
@@ -1,8 +1,8 @@
 describe('Login into MangoApps', () => {
   it('Visits Login page of the app and tries to login', () => {
     cy.visit('/');
-    cy.get('#user_id').click().type('admin@pages.com');
-    cy.get('#password').click().type('temp1234');
+    cy.get('#user_id').click().type('admin@riadomain.com');
+    cy.get('#password').click().type('Mango@123');
     cy.get('#yui-gen0').click();
   });
 });
