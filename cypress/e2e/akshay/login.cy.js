d2769c0e5389834c8a4238d94a4514317acb8dec
akshayp-mangospring <akshayp@mangospring.com>
2022-06-03 12:48:12 +0530

    - Add tests for checking if the login flow works properly - Add test for checking the background color of the header after login

--- cypress/e2e/akshay/login.cy.js
+++ cypress/e2e/akshay/login.cy.js
@@ -1,8 +1,27 @@
 describe('Login into MangoApps', () => {
-  it('Visits Login page of the app and tries to login', () => {
+  it('Visits Login page of the app and tries to login with invalid details', () => {
+    cy.visit('/');
+    cy.get('#user_id').click().type('admin@riadomain.com');
+    cy.get('#password').click().type('@123{enter}');
+  });
+
+  it('Checks if error is visible or not', () => {
+    cy.get('#error_status').should('be.visible');
+  });
+
+  it('Visits Login page of the app and tries to login with valid details', () => {
     cy.visit('/');
     cy.get('#user_id').click().type('admin@riadomain.com');
     cy.get('#password').click().type('Mango@123');
     cy.get('#yui-gen0').click();
+  });
+
+  it('Checks color of the top header to be black', () => {
+    cy.get('.ms-main-header-top')
+      .should('have.css', 'background')
+      .and(
+        'eq',
+        'rgb(51, 60, 73) none repeat scroll 0% 0% / auto padding-box border-box'
+      );
   });
 });
