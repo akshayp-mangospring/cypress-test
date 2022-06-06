a06a835f954074c82545d8c451f80e903886edde
Nisha T <89187411+nishatatmango@users.noreply.github.com>
2022-06-06 10:13:25 +0530

    add news feed test case

--- cypress/e2e/nisha/login.cy.js
+++ cypress/e2e/nisha/login.cy.js
@@ -18,23 +18,7 @@ describe("login", () => {
     
     });
 
-  it("create feed", () => {
-    cy.visit("/");
-    cy.url().should("include", "riadomain"); 
-    cy.get("#user_id").click().type("admin@riadomain.com"); 
-    cy.get("#password").click().type("Mango@123"); 
-    cy.get("#yui-gen0").click();
-    cy.get(".ico_arrow_main").click();                                                  //click menu option
-    cy.get("#lhs_container > li.master-lhs.lhs-dd-parent.active.inbox > a").click();   // click news feed
-    cy.get(".post-in-team.ma-s-theme")
-      .should("be.visible").click();
-   // cy.get("#project_status_update_team").type("test automation");  
-   
-
-
-
-   });
-
+  
   });
 
 
--- cypress/e2e/nisha/newsfeed.cy.js
+++ cypress/e2e/nisha/newsfeed.cy.js
@@ -5,19 +5,32 @@ describe("Feeds", () => {
  it("create feed", () => {
     cy.visit("/");
 //  cy.url().should("include", "riadomain");
-//  cy.fixture('example.json').then(data => {
+//  cy.fixtures('example.json').then(data => {
     cy.get("#user_id").click().type("admin@riadomain.com");
     cy.get("#password").click().type("Mango@123");
     cy.get("#yui-gen0").click();
     cy.get(".ico_arrow_main").click();                                                                               //click menu option
     cy.get("#lhs_container > li.master-lhs.lhs-dd-parent.active.inbox > a > span:nth-child(2)").click();               // click news feed
     cy.get(".post-in-team.ma-s-theme").should("be.visible").click();
+    cy.wait(2000);
     cy.get("#token-input-myTeams").click();
 
-    //Radio buttons(share with)
-//   cy.get("#labelTypeCustom").should("be.visible").should("be.checked");                   //all teams
+    //Radio buttons(share with followers)
     cy.get("#allMyFollowers").should("be.visible").should("not.be.checked").click();        // followers
     cy.get("#project_status_update").type("test automation");
-    cy.get("#ms-feed-btn > span > span").click();                                                        //share
+    cy.get("#ms-feed-btn > span > span").click();                                           //share
+    cy.get("primary_tab").click();
     });
+
+
+//    //Radio buttons(share with teams)
+//     cy.get("#ms-feed-btn > span > span").click();                                           //share
+//     cy.get("#project_status_update").type("test automation");
+//     cy.get("#ms-feed-btn > span > span").click();  
+//     });
+
+
+
+
+    
 });
